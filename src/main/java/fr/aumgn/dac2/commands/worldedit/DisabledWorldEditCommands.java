b047535682752a16c253fd4fe3597d1fd7af7219
GibbsFrance <ngaillard.vosges@gmail.com>
2013-02-28 17:40:50 +0100

    Eclipse n'aime pas strictFlags. Bug ?

--- src/main/java/fr/aumgn/dac2/commands/worldedit/DisabledWorldEditCommands.java
+++ src/main/java/fr/aumgn/dac2/commands/worldedit/DisabledWorldEditCommands.java
@@ -15,12 +15,12 @@ public class DisabledWorldEditCommands extends DACCommands {
         super(dac);
     }
 
-    @Command(name = "setwe", max = -1, strictFlags = false)
+    @Command(name = "setwe", max = -1)
     public void setwe(CommandSender sender, CommandArgs args) {
         sender.sendMessage(msg("disabled"));
     }
 
-    @Command(name = "select", max = -1, strictFlags = false)
+    @Command(name = "select", max = -1)
     public void select(CommandSender sender, CommandArgs args) {
         sender.sendMessage(msg("disabled"));
     }
